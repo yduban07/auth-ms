a344500ae1da27884b2a0740f0f9ad1d8c2fb5ca
yduban07 <yduban27@gmail.com>
2025-01-17 21:38:48 -0600

    prisma client added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit, Logger } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class AuthService {}
+export class AuthService extends PrismaClient implements OnModuleInit {
+    private readonly logger =  new Logger('AuthService');
+
+    async onModuleInit() {
+        await this.$connect();
+        this.logger.log('Mongodb connected');
+    }
+
+
+
+}
